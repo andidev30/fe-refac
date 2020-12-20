cc66fed41bc602f4ec403ddb7256d1ed339cdeaa
andidev30 <aandi30082001@gmail.com>
2020-12-21 05:00:25 +0700

    inital

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+import {Container, Button, Image, Row, Col} from "react-bootstrap"
+import { useMediaQuery } from 'react-responsive';
+
+import NavbarCustom from "../components/NavbarCustom"
+import dataPartner from "../data/partner.json"
 
 export default function Home() {
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Refactory</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div>
+        <div id="hero-image" style={{backgroundColor: "#3e789f", minHeight: "100vh"}}>
+          <Container>
+            <NavbarCustom menu={["Home", "Courses", "custom training", "case studies", "blog" ]} />
+            <Container style={isMobile ? {textAlign: "center", paddingTop: 50 } : {padding: "80px 100px 0px 50px" }}>
+              <h1 style={{color: "white", fontWeight: "bold"}}>
+                Empowering <span style={{color: "orange"}}>People</span><br /> Through Programming
+              </h1>
+              <h5 style={{color: "white", maxWidth: "50%", paddingTop: 10, fontWeight: 'inherit'}}>
+                Refactory adalah perusahaan edukasi dan teknologi yang menyediakan layanan lengkap berupa course maupun custom training yang materinya dapat disesuaikan dengan kebutuhan teknologi dan bisnis perusahaan Anda.
+              </h5>
+              <div style={{paddingTop: 20}}>
+                <Button style={{ color: "white !important", marginRight: 20}} className="btn btn-warning rounded color-white">Temukan Solusi Anda</Button>
+                <Button style={{color: "white !important"}} className="btn btn-outline-warning rounded">Tingkatkan Skill Anda</Button>
+              </div>
+              <div style={{marginTop: 50, textAlign: "center", color: "white",}}>
+                <h3>PARTNER EKSLUSOF KAMI</h3>
+                <div style={{display: "flex", justifyContent: "space-between", marginTop: 30}}>
+                  {
+                    dataPartner.data.map((partner) => (
+                      <Image key={partner.id} height={50} src={partner.photo_url} />
+                    ))
+                  }
+                </div>
+              </div>
+            </Container>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </Container>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div style={{marginTop: 80}}>
+          <Container>
+            <h2 style={{ fontWeight: "bold", textAlign: "center"}}>Apa Yang Refactory Dapat Bantu?</h2>
+            <Row style={{marginTop: 40, textAlign: "center"}}>
+              <Col md="6">
+                <h4 style={{fontWeight: "bold"}}>Memperkuat Tim Engineer Anda</h4>
+                <p style={{paddingTop: 10}}>
+                  Bisnis di jaman modern membutuhkan keterampilan pengembangan terbaik untuk meningkatkan skala produk. Kami dapat mempersiapkan course dan juga dapat menyediakan tim yang menangani kebutuhan digital Anda.
+                </p>
+              </Col>
+              <Col md="6">
+                <h4>Memperkuat Tim Engineer Anda</h4>
+                <p style={{paddingTop: 10}}>
+                  Kami adalah perusahaan One-Stop IT Solution untuk proyek Anda, membantu di setiap tahap mulai dari menyusun ide, melalui desain dan pengembangan aplikasi seluler, situs web dan aplikasi desktop, hingga peluncuran produk.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
+        <div style={{marginTop: 100, textAlign: "center"}}>
+          <h6>INSIGHT TERBARU</h6>
+          <h4 className="font-weight-bold pt-2">Knowledge Sharing</h4>
+        </div>
+
+        <div style={{backgroundColor: "grey", padding: 50, marginTop: 100}}>
+          <h2 style={{textAlign: "center"}}>AS SEE ON</h2>
+          <div style={{display: "flex", justifyContent: "center", paddingTop: "30px"}}> 
+            {
+              dataPartner.data.map((partner) => (
+                <Image key={partner.id} height={isMobile ? 30 : 50} src={partner.photo_url} />
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
